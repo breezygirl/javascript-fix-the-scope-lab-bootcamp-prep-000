08643e3c86ef01165058a76d9828dc575e6e7141
Beerscheba C <designergo@aol.com>
2018-06-06 14:54:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var animal = 'dog'
 
+<<<<<<< HEAD
 function myAnimal(animal) {
  animal = 'dog'
   return animal
+=======
+function myAnimal() {
+ var animal = cat;
+  return myAnimal
+>>>>>>> ac8f180b051605722d5310cbd873753f8546aabf
 }
 
 function yourAnimal(animal) {
@@ -10,7 +16,7 @@ function yourAnimal(animal) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return yourAnimal
 }
 
 function add2(n) {
@@ -19,7 +25,11 @@ function add2(n) {
   
 
   // Feel free to move things around!
+<<<<<<< HEAD
   return (n+ 2);
+=======
+  return 2;
+>>>>>>> ac8f180b051605722d5310cbd873753f8546aabf
 }
 
 var funkyFunction = function(theFunk) {
@@ -30,7 +40,12 @@ var funkyFunction = function(theFunk) {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
+<<<<<<< HEAD
 var theFunk = "FUNKY!"
+=======
+var theFunk = funkyFunction
+
+>>>>>>> ac8f180b051605722d5310cbd873753f8546aabf
 
 
 
